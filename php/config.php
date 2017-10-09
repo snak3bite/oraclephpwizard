1857df0320df36770839ed904ee448ea2e986ed9
snak3bite <jonas.jberglund@gmail.com>
2017-10-09 17:11:45 +0200

    added a few constants

--- php/config.php
+++ php/config.php
@@ -4,12 +4,27 @@ define( "DB_PASSWORD", "opw" );
 define( "DB_CONNSTR", "localhost/XE" );
 
 define( "COMPANY", "KXE Styrteknik AB" );
-define( "FNC_SEPARATOR","//------------------------------------------------------------------");
 
-// To change indentation alter value for IND1 -IND3
+// Path and filename to default definitions of default sizes for oracle types
+define( "DEFSIZELOCATION", "/var/www/html/projects/oraclephpwizard/php/defsizes.php" );
+
+// Prefix for oracle types default size constants
+// if this is changed all constant names must be chanchged as well
+define( "ORATYPESIZEPREFIX", "SIZE_" );
+
+define ( "FILENAME_LCASE", true );
+define ( "CLASSNAME_INITCAP", true );
+
+// To change indentation alter value for IND1
 define( "IND1", "  " );
-define( "IND2", "    " );
-define( "IND3", "      " );
+define( "IND2", IND1 . IND1 );
+define( "IND3", IND1 . IND2 );
+
+// Newline and blankline constants for class
 define( "BLANKLINE", "\n");
-define( "NL", "\n"); // Newline
+define( "NL", "\n");
+
+// Newline and blankline constants for skeleton
+define( "SBLANKLINE", "\n");
+define( "SNL", "\n");
 ?>
