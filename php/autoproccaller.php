fb57437cb717c2878e69e3bdceff39a678eace65
snak3bite <jonas.jberglund@gmail.com>
2017-10-14 20:14:00 +0200

    Added copyright/license notice

--- php/autoproccaller.php
+++ php/autoproccaller.php
@@ -1,5 +1,22 @@
 <?php
+/*
+  Copyright (C) 2017 Jonas Berglund <jonas.berglund@kxe.se>
 
+  This file is part of Oracle PHP Wizard (OPW).
+
+  OPW is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPW is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPW.  If not, see <http://www.gnu.org/licenses/>.
+*/
 require_once("config.php");
 
 class AutoProcCaller {
